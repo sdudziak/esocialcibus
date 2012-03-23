ef7c9370c1e4f02093398e8411846d0394236e19
Simone Gentili <sensorario@gmail.com>
2012-03-23 11:32:45 +0100

    first commit

---
+++ esocialcibus/ESocialCibus.php
@@ -0,0 +1,19 @@
+<?php
+
+class ESocialCibus extends CWidget
+{
+    public $width = '224';
+    public $height = '224';
+    public $shadow = true;
+    public $linkColor = 'e95b24';
+
+    public function run()
+    {
+        echo '<iframe src="http://socialcibus.com/widget?width=' . ($this->shadow ? $this->width : ($this->width - 4)) . '&link_color=' . $this->linkColor . '" '
+        . 'scrolling="no" '
+        . 'frameBorder="0" '
+        . 'style="border-width: 0; height: ' . $this->height . 'px; width: ' . $this->width . 'px;">'
+        . '</iframe>';
+    }
+
+}
